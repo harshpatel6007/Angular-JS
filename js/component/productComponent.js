c5647b1483a3f249cee6ec5f1aece0cd2217f971
Harsh Patel <harshpatel6007@gmail.com>
2017-08-12 19:13:59 +0530

    add another component to view product information

--- js/component/productComponent.js
+++ js/component/productComponent.js
@@ -1,3 +1,14 @@
+function ProductCtrl($scope) {
+    console.log("inside product controller ");
+    console.log("$ctrl.products" + this.products[0].title)
+
+    this.deleteProduct = function (product) {        
+        var index = this.products.indexOf(product);
+        console.log("remove product on index : " + index);
+        this.products.splice(index, 1);
+    }
+}
+
 myAppComponents.component('productDetail', {        
     // template : function () {
     //     return '<div> this is product template </div>'
@@ -5,6 +16,10 @@ myAppComponents.component('productDetail', {
     templateUrl : 'partials/products.html',
     controller : ProductCtrl,
     bindings : {
-        products: '='
+        products : '='
     }    
 });
+
+
+
+// myAppControllers.controller('ProductCtrl', ProductCtrl());
---
+++ js/component/productInfoComponent.js
@@ -0,0 +1,11 @@
+function PrdouctInfoCtrl() {
+    
+}
+
+myAppComponents.component('productInfo', {
+    templateUrl : 'partials/productInfo.html',
+    controller : PrdouctInfoCtrl,
+    bindings : {
+        products : '='
+    }
+})
